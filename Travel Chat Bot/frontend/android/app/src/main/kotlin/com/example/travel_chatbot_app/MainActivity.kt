0d1ba1d80b60bcf6ba20902588c15841ff88ec2a
vphong3904 <chauvinhphonh2004@gmail.com>
2026-06-28 15:38:17 +0700

    merge frontend admin

--- Travel Chat Bot/frontend/android/app/src/main/kotlin/com/example/travel_chatbot_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travel_chatbot_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Travel Chat Bot/frontend/lib/services/travel_api.dart
+++
@@ -1,3 +0,0 @@
-export 'travel_api_service.dart';
-export 'api_service.dart';
-export 'sse_client.dart';
